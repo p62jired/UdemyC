fe648e4f1a93ca55a4c9315ffab346cb4682fc90
p62jired <jr88diegol@gmail.com>
2022-02-11 15:11:12 +0100

    update ignore

---
+++ 0_LerningByDoing/Array.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    float vector1[3]{};
+    float vector2[3]{};
+    std::cout << "Enter values for vector 1: " << std::endl;
+    for (unsigned int i = 0; i < 3; i++)
+    {
+        std::cin >> vector1[i];
+    }
+    std::cout << "Enter values for vector 2: " << std::endl;
+    for (unsigned int i = 0; i < 3; i++)
+    {
+        std::cin >> vector2[i];
+    }
+    for (unsigned int i = 0; i < 3; i++)
+    {
+
+        std::cout << "vector1[i] + vector2[i] = " << vector1[i] + vector2[i] << std::endl;
+    }
+
+    return 0;
+}
